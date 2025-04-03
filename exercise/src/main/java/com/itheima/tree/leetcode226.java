1b87177d381cc3891f5b62f5d73b4ba344b541df
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-04-04 00:33:46 +0800

    反转二叉树

--- exercise/src/main/java/com/itheima/tree/leetcode226.java
+++ exercise/src/main/java/com/itheima/tree/leetcode226.java
@@ -1,4 +1,18 @@
 package com.itheima.tree;
 
 public class leetcode226 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
 }
