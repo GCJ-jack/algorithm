64a682b10f2ae1e9ee93c169883371c904f34e7b
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-04-04 13:21:10 +0800

    102. 二叉树的层序遍历

---
+++ exercise/src/main/java/com/itheima/tree/leetcode102.java
@@ -0,0 +1,44 @@
+package com.itheima.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+
+
+
+
+public class leetcode102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+
+        LinkedList<Object> queue = new LinkedList<>(); // 使用 LinkedList 实现 Queue
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> levelNode = new ArrayList<>();
+
+            for(int i=0;i<levelSize;i++){
+                TreeNode currentNode = queue.poll();
+                levelNode.add(currentNode.val);
+
+                if(currentNode.left!=null){
+                    queue.add(currentNode.left);
+                }
+
+                if(currentNode.right!=null){
+                    queue.add(currentNode.right);
+                }
+            }
+
+            result.add(levelNode);
+        }
+        return result;
+    }
+}
