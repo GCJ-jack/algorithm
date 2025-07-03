18977aa7b40b0bb970910108e89cbeff55a86be5
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-03 22:47:47 +0800

    boxing

---
+++ exercise/src/main/java/com/itheima/boxing/Boxing.java
@@ -0,0 +1,11 @@
+package com.itheima.boxing;
+
+public class Boxing {
+    public static void main(String[] args) {
+         int a = Integer.parseInt("5");
+
+         int b = Integer.valueOf("3");
+
+        System.out.println(b);
+    }
+}
