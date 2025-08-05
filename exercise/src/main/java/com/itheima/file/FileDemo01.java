e515ab796facb44bdbe87ccd33117df89bf0fe69
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-08-05 22:53:48 +0800

    listfiles 完成

--- exercise/src/main/java/com/itheima/file/FileDemo01.java
+++ exercise/src/main/java/com/itheima/file/FileDemo01.java
@@ -8,10 +8,28 @@ public class FileDemo01 {
     public static void main(String[] args) throws IOException {
 
 
-        File file = new File("/Users/guochaojun/Desktop/algorithm","files");
+//        File file = new File("/Users/guochaojun/Desktop/algorithm","files");
+//
+//        boolean a =  file.mkdirs();
+//
+//        System.out.println(a);
 
-        boolean a =  file.mkdirs();
 
-        System.out.println(a);
+        for (int i = 0; i < 5; i++) {
+            File  file = new File("/Users/guochaojun/Desktop/algorithm/files","file"+String.valueOf(i)+".txt");
+            boolean res = file.createNewFile();
+            System.out.println(res);
+        }
+
+        File file = new File("/Users/guochaojun/Desktop/algorithm/files");
+
+        File[] files = file.listFiles();
+
+
+        for (File filess: files) {
+            if(filess.getName().endsWith("txt")){
+                System.out.println("this is the file number " + filess.getName());
+            }
+        }
     }
 }
