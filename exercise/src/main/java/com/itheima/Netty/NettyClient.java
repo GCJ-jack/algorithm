8f4d47671314326f2026202dcf30a5e4704b61ab
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-09-15 15:21:15 +0800

    netty 试图连接

--- exercise/src/main/java/com/itheima/Netty/NettyClient.java
+++ exercise/src/main/java/com/itheima/Netty/NettyClient.java
@@ -1,4 +1,46 @@
 package com.itheima.Netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 public class NettyClient {
+
+    public static void main(String[] args) {
+
+        Bootstrap bootstrap = new Bootstrap().group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new StringEncoder())
+                                .addLast(new StringDecoder())
+                                .addLast(new SimpleChannelInboundHandler<String>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext ctx,String msg) throws Exception{
+                                        System.out.println(msg);
+                                    }
+                                });
+                    }
+                });
+
+
+        ChannelFuture channelFuture = bootstrap.connect("localhoust", 8082);
+        channelFuture.addListener(f->{
+            if(f.isSuccess()){
+                System.out.println("连接成功");
+
+            }else{
+                System.out.println("连接失败");
+            }
+        });
+
+    }
 }
--- exercise/src/main/java/com/itheima/Netty/NettyServer.java
+++ exercise/src/main/java/com/itheima/Netty/NettyServer.java
@@ -1,4 +1,42 @@
 package com.itheima.Netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 public class NettyServer {
+
+    public static void main(String[] args) {
+
+        //创建一个hashmap 来存储数据
+
+        //创建 bootstrap 组件
+        ServerBootstrap serverBootstrap = new ServerBootstrap().group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception{
+                        ch.pipeline().addLast(new LineBasedFrameDecoder(1024))
+                                .addLast(new StringDecoder())
+                                .addLast(new StringEncoder());
+                        //加入相对应的回应处理器
+                        //以及这个 db 处理器
+                    }
+                });
+
+        ChannelFuture bindFuture = serverBootstrap.bind(8082);
+        bindFuture.addListener(f ->{
+            if(f.isSuccess()){
+                System.out.println("服务器成功监听");
+            }else{
+                System.out.println("服务器监听失败");
+            }
+        });
+    }
 }
