b6157ba45b0f1b950fb5cf14f5fd6e50a3b17157
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-09-23 17:17:04 +0800

    完成beandefinition

--- mini_spring/src/main/java/com/heima/BeanDefinition.java
+++ mini_spring/src/main/java/com/heima/BeanDefinition.java
@@ -1,9 +1,15 @@
 package com.heima;
 
+import com.heima.annotation.Autowired;
+import com.heima.annotation.Component;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BeanDefinition {
 
@@ -39,6 +45,22 @@ public class BeanDefinition {
 
 
     public BeanDefinition(Class<?> clazz){
+        this.beanType = clazz;
+        Component component = clazz.getDeclaredAnnotation(Component.class);
+        this.name = component.name().isEmpty()? clazz.getSimpleName() : component.name();
 
+        try {
+            this.constructor = clazz.getConstructor();
+            this.postConstructMethod = Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(m -> m.isAnnotationPresent(PostConstruct.class))
+                    .findFirst()
+                    .orElse(null);
+
+            this.autowiredFields = Arrays.stream(clazz.getDeclaredFields())
+                    .filter(m -> m.isAnnotationPresent(Autowired.class))
+                    .collect(Collectors.toList());
+        }catch (NoSuchMethodException e){
+            throw new RuntimeException(e);
+        }
     }
 }
