0a2d84e27f9a5242ee78cc0a2bf2c2c78b297c96
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-09-23 23:24:25 +0800

    更新

---
+++ mini_spring/src/main/java/com/heima/MyBeanPostProcessor.java
@@ -0,0 +1,13 @@
+package com.heima;
+
+import com.heima.annotation.Component;
+
+@Component
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object afterInitializeBean(Object bean, String beanName) {
+        System.out.println(beanName + "初始化完成");
+        return bean;
+    }
+}
