cb3c52fb2a56cafd6cafd2dd99b656774c36daba
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-09-23 17:05:31 +0800

    创建构造器

--- mini_spring/src/main/java/com/heima/BeanDefinition.java
+++ mini_spring/src/main/java/com/heima/BeanDefinition.java
@@ -1,4 +1,44 @@
 package com.heima;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class BeanDefinition {
+
+    private final String name;
+    private final Constructor<?> constructor;
+    private final Method postConstructMethod;
+    private final List<Field> autowiredFields;
+    private final Class<?> beanType;
+
+    public String getName() {
+        return name;
+    }
+
+    public BeanDefinition(String name) {
+        this.name = name;
+    }
+
+    public Constructor<?> getConstructor() {
+        return constructor;
+    }
+
+    public Method getPostConstructMethod() {
+        return postConstructMethod;
+    }
+
+    public List<Field> getAutowiredFields() {
+        return autowiredFields;
+    }
+
+    public Class<?> getBeanType() {
+        return beanType;
+    }
+
+
+    public BeanDefinition(Class<?> clazz){
+
+    }
 }
