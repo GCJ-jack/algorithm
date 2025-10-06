d7cddfd0858f5d83b3d37d485a1593fd68aaa339
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-10-07 00:44:57 +0800

    finish dijkstra again

--- samsung/src/main/java/NaiveDijkstra.java
+++ samsung/src/main/java/NaiveDijkstra.java
@@ -7,41 +7,41 @@ public class NaiveDijkstra {
 
         Scanner scanner = new Scanner(System.in);
 
-        //输入节点的数量以及边的数量
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        int[] minDist = new int[n + 1];
+        int[][] grid = new int[n + 1][n + 1];
 
-        int[][] grid = new int[n+1][n+1];
-
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             Arrays.fill(grid[i],Integer.MAX_VALUE);
         }
 
-        for (int i = 0; i < m; i++) {
-            int node1 = scanner.nextInt();
-            int node2 = scanner.nextInt();
-            int value = scanner.nextInt();
-
-            grid[node1][node2] = value;
-        }
-
-        Arrays.fill(minDist,Integer.MAX_VALUE);
         boolean[] visited = new boolean[n + 1];
+
+        int[] minDist = new int[n + 1];
+
+        Arrays.fill(minDist, Integer.MAX_VALUE);
 
         int start = 1;
         int end = n;
 
         minDist[start] = 0;
 
+        for (int i = 0; i < m; i++) {
+            int node1 = scanner.nextInt();
+            int node2 = scanner.nextInt();
+            int val = scanner.nextInt();
+
+            grid[node1][node2] = val;
+        }
+
         for (int i = 1; i <= n; i++) {
 
-            int minVal = Integer.MAX_VALUE;
             int cur = 1;
-
-            for (int j = 1; j <=n ; j++) {
-                if(!visited[j] && minDist[j] < minVal){
+            int minVal = Integer.MAX_VALUE;
+            //选择最近的节点
+            for (int j = 1; j <= n; j++) {
+                if(!visited[j]&&minDist[j] < minVal){
                     cur = j;
                     minVal = minDist[j];
                 }
@@ -49,13 +49,13 @@ public class NaiveDijkstra {
 
             visited[cur] = true;
 
-            //更新
             for (int j = 1; j <= n; j++) {
-                if(!visited[j]&& grid[cur][j] != Integer.MAX_VALUE &&  minDist[cur] + grid[cur][j] < minDist[j]){
-                    minDist[j] = minDist[cur] + grid[cur][j];
+                if(!visited[j] && grid[cur][j] != Integer.MAX_VALUE && grid[cur][j] + minDist[cur] < minDist[j]){
+                    minDist[j]= grid[cur][j] + minDist[cur];
                 }
             }
         }
+
 
         if (minDist[end] == Integer.MAX_VALUE) {
             System.out.println(-1); // 不能到达终点
@@ -63,5 +63,4 @@ public class NaiveDijkstra {
             System.out.println(minDist[end]); // 到达终点最短路径
         }
     }
-
 }
