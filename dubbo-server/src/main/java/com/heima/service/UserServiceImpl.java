862f83f6feaf9baa0276340d83fe0aa2674ae8b4
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-11-03 16:59:22 +0800

    impl

---
+++ dubbo-server/src/main/java/com/heima/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.heima.service;
+
+import com.heima.model.User;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public User getUser(int id) {
+        return null;
+    }
+}
