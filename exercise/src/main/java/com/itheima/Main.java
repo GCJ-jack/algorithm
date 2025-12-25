60e85452b60acbaa1ebefbb7e18d617ce9868906
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-12-25 18:34:09 +0800

    shortest distance

--- exercise/src/main/java/com/itheima/Main.java
+++ exercise/src/main/java/com/itheima/Main.java
@@ -1,6 +1,32 @@
 package com.itheima;
 
 public class Main {
+
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+
+        int left = -1;
+        int right = - 1;
+
+        int distance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < wordsDict.length; i++) {
+            if(wordsDict[i].equals(word1)){
+                left = i;
+            }
+
+            if(wordsDict[i].equals(word2)){
+                right = i;
+            }
+
+            if (left != -1 && right != -1) {
+                distance = Math.min(distance, Math.abs(right - left));
+            }
+
+
+        }
+
+        return distance;
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
