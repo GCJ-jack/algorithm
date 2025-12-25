fba4e6a4c9059e2e337d200f576b39c3777d4c57
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-12-26 00:28:26 +0800

    finished the problem

--- exercise/src/main/java/com/itheima/String/CanPermutePalindrome.java
+++ exercise/src/main/java/com/itheima/String/CanPermutePalindrome.java
@@ -6,17 +6,34 @@ import java.util.Set;
 public class CanPermutePalindrome {
 
     public boolean canPermutePalindrome(String s){
-        Set<Character> set = new HashSet<>();
+//        Set<Character> set = new HashSet<>();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            if(set.contains(s.charAt(i))){
+//                set.remove(s.charAt(i));
+//            }else{
+//                set.add(s.charAt(i));
+//            }
+//        }
+//
+//        return set.size() <= 1;
+        
+        int[] cnt = new int[128];
 
         for (int i = 0; i < s.length(); i++) {
-            if(set.contains(s.charAt(i))){
-                set.remove(s.charAt(i));
-            }else{
-                set.add(s.charAt(i));
-            }
+            cnt[s.charAt(i)]++;
         }
 
-        return set.size() <= 1;
+        int odd = 0;
+
+        for(int x:cnt){
+            if((x & 1) == 1){
+                odd++;
+            }
+            if(odd > 1) return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
