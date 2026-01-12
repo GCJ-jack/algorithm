7beb2bc18345bbd0bc19a74997677badbfa2075b
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2026-01-12 14:23:06 +0800

    分别使用atomic interger 还有synchronize 悲观锁来保证count 数字增加的准确性

---
+++ CodeWIthGpt/src/main/java/com/chaojun/TaskOne.java
@@ -0,0 +1,39 @@
+package com.chaojun;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TaskOne {
+    //这是第一种解决方案
+    //使用atomic integer来确保原子性
+    //同一时间只有一个线程能够来操纵这个对象
+//    static AtomicInteger count = new AtomicInteger(0);
+
+    //第二种方法使用 synchronize来确保只有一个线程来操作这个数字
+    static int count = 0;
+
+    static Object key = new Object();
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(()->{
+//                count.incrementAndGet();
+                synchronized (key){
+                    count++;
+                }
+            });
+        }
+
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()){
+            Thread.sleep(10);
+
+        }
+        System.out.println("最终结果：" + count);
+
+    }
+}
