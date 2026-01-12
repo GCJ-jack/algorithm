2949d40b04df0d6c66dc8434b5631508f3aeedb8
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2026-01-12 22:08:09 +0800

    simple nio server

---
+++ CodeWIthGpt/src/main/java/com/chaojun/NioServer.java
@@ -0,0 +1,65 @@
+package com.chaojun;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.security.Key;
+import java.util.Iterator;
+
+public class NioServer {
+
+    static int port  = 8083;
+
+    public static void main(String[] args) throws IOException {
+
+        Selector selector = Selector.open();
+
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+
+        ssc.configureBlocking(false);
+
+        ssc.bind(new InetSocketAddress(port));
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (true){
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.keys().iterator();
+
+            while (iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+
+                if(selectionKey.isAcceptable()){
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+
+                    SocketChannel client = serverSocketChannel.accept();
+                    client.configureBlocking(false);
+                    client.register(selector, SelectionKey.OP_READ);
+                }
+
+
+                if(selectionKey.isReadable()){
+                    SocketChannel client = (SocketChannel) selectionKey.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+                    if(client.read(byteBuffer) != -1){
+                        System.out.println(client.getRemoteAddress() + " 断开连接了");
+                        client.close();
+                    }else{
+                        byteBuffer.flip();
+                        byte[] bytes = new byte[byteBuffer.remaining()];
+                        byteBuffer.get(bytes);
+                        System.out.println(new String(bytes));
+                        byteBuffer.clear();
+                    }
+                }
+            }
+        }
+
+    }
+}
